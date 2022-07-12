17056930c9aecc50a6e91a8a78376aedca787436
Manueil86 <manuelcastro.work@gmail.com>
2022-07-11 22:43:27 -0300

    agrego figuras html y su js

---
+++ figuras.js
@@ -0,0 +1,35 @@
+// Código del cuadrado
+console.group("Cuadrados");
+
+const ladoCuadrado = 5;
+
+console.log("Los lados del cuadrado miden: " + ladoCuadrado + "cm");
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log("El perimetro del cuadrado es: " + perimetroCuadrado + "cm");
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+console.log("El area del cuadrado es: " + areaCuadrado + "cm^2");
+
+console.groupEnd();
+
+// Código del triángulo
+console.group("Triángulos");
+
+
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const baseTriangulo = 4;
+
+    console.log(
+        "Los lados del triángulo miden: " 
+        + ladoTriangulo1 
+        + "cm, " 
+        + ladoTriangulo2 
+        + "cm, " 
+        + baseTriangulo 
+        + "cm"
+        );
+
+
+console.groupEnd();
